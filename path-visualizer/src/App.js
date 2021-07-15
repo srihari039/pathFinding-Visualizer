8d0dd264a535cbb3bff07c476e57dc4427bb4f83
sri <sriharimalla001@gmail.com>
2021-07-15 20:04:53 +0530

    intitial Commit

---
+++ path-visualizer/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Card from './components/upper-panel/Card';
+
+
+function App() {
+  return (
+    <div>
+      <Card>
+        
+      </Card>
+
+      <Card>
+
+      </Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ path-visualizer/src/components/upper-panel/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Card.css'
+
+const Card = () => {
+	return (
+		<div className='card__'>
+			text
+		</div>
+	)
+}
+
+export default Card;
